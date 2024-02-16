49285337bc858ee44814ef50c41448b91710f89f
Gregory Karastergios <gregory@gregnk.com>
2024-02-16 16:24:43 -0500

    Bumped copy year

--- file_audit.py
+++ file_audit.py
@@ -1,5 +1,5 @@
 '''
-(c) 2023 Gregory Karastergios
+(c) 2023-2024 Gregory Karastergios
 
 Permission to use, copy, modify, and/or distribute this software for any
 purpose with or without fee is hereby granted, provided that the above
